a381213db22d9f2967e7a973034360f456513a95
Henry Ni <henryni914@gmail.com>
2021-06-10 18:05:32 -0400

    created a navbar and temp links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Landing from './pages/Landing'
+import Navbar from './components/Navbar';
+import Landing from './pages/Landing';
+import About from './pages/About'
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <Route exact path="/" component={Landing} />
+        <Route exact path="/about" component={About} />
       </div>
     </Router>
   );
---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About (){
+
+    return(
+        <div>
+            <h3>About Us Section</h3>
+        </div>
+    )
+}
