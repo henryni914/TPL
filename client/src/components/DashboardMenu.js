efc237468edb318ce2bb8e5f3dd36f11dfaf5352
Henry Ni <henryni914@gmail.com>
2021-06-16 17:28:52 -0400

    restructured directory

--- client/src/components/DashboardMenu.js
+++ client/src/components/DashboardMenu.js
@@ -46,14 +46,14 @@ export default function DashboardMenu() {
             <>
                 <GridItem colSpan={1}
                 // bg='grey'
+                align={'left'}
                 >
                     <Stack direction={'column'} spacing={4}>
                         {
                             dashboardTabs.map((tab) => (
-                                <Box key={tab}>
+                                <Box key={tab} >
                                     <Link
                                         p={2}
-                                        // to={ }
                                         fontSize={'md'}
                                         onClick={() => handleTabChange(tab)}
                                     >
@@ -70,8 +70,8 @@ export default function DashboardMenu() {
 
     return (
         <Box
-            bg={useColorModeValue('white', 'gray.800')}
-            color={useColorModeValue('gray.600', 'white')}
+            // bg={useColorModeValue('white', 'gray.800')}
+            // color={useColorModeValue('gray.600', 'white')}
             py={{ base: 4 }}
             px={{ base: 0 }}
             align={'center'}
@@ -82,6 +82,7 @@ export default function DashboardMenu() {
                 templateColumns="repeat(5, 1fr)"
                 gap={4}
                 display={{ base: 'none', md: 'grid' }}
+                bg='blue'
             >
                 <DesktopDash />
                 <GridItem colSpan={4}
