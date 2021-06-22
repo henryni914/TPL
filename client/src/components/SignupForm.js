a895293d842b9c235d7b81b3e0d67415e7a5625f
Henry Ni <henryni914@gmail.com>
2021-06-22 17:24:46 -0400

    changed sign up to chakra ui components

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,24 +1,57 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    Container,
+    Heading,
+    Button,
+} from "@chakra-ui/react"
 
 export default function SignupForm() {
 
     let username = useRef(null);
     let email = useRef(null);
+    let password = useRef(null);
+    const [submitting, setSubmitting] = useState(false)
+
 
     function handleSubmit() {
+        setSubmitting(true)
+        setTimeout(() => {
+            setSubmitting(false)
+        }, 1000)
         console.log(username.current.value)
         console.log(email.current.value)
+        console.log(password.current.value)
     }
 
     return (
-        <>
-            <h2>Sign up</h2>
-            <h4>Username</h4>
-            <input ref={username}></input>
-            <h4>Email</h4>
-            <input ref={email}></input>
-            <br></br>
-            <button onClick={() => handleSubmit()}>Test</button>
-        </>
+        <Container py={{ base: 4 }}>
+            <Heading as="h4" size="md">Sign Up Form</Heading>
+            <FormControl id="email" isRequired >
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" ref={email} placeholder="example@domain.com" />
+                <FormHelperText>We'll never share your email.</FormHelperText>
+                <FormLabel>Username</FormLabel>
+                <Input type="username" ref={username} />
+                <FormHelperText>Please enter a username.</FormHelperText>
+                <FormLabel>Password</FormLabel>
+                <Input type="password" ref={password} />
+                <FormHelperText>Please enter a password.</FormHelperText>
+                <FormErrorMessage></FormErrorMessage>
+                <Button
+                    mt={4}
+                    colorScheme="teal"
+                    type="submit"
+                    onClick={() => handleSubmit()}
+                    isLoading={submitting}
+                >
+                    Submit
+                </Button>
+            </FormControl>
+        </Container>
     )
 }
